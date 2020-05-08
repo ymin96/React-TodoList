615bf17a62991fa872ccdb7c2c6f32bf5992c042
Kim YongHwan <ymin96@naver.com>
2020-05-08 17:14:43 +0900

    p. 318

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './styles/main.module.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+import modules from './modules';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const store = createStore(modules, window.devToolsExtension && window.devToolsExtension());
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
